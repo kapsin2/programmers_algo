eae8120bea81e0d5215af368b726c777b535f728
kapsin2 <110449509+kapsin2@users.noreply.github.com>
2023-01-26 11:40:36 +0900

    CorrectParentheses2

--- CorrectParentheses.java
+++ CorrectParentheses.java
@@ -1,2 +1,15 @@
-package com.sparta.h2test;public class CorrectParentheses {
+package com.sparta.h2test;
+
+public class CorrectParentheses {
+    boolean solution(String s) {
+        int a = 0;
+        for(int i = 0; i< s.length();i++) {
+            if((int)s.charAt(i) == 40) a += 1;
+            else if ((int)s.charAt(i) == 41) a -= 1;
+
+            if(a<0) return false;
+        }
+        if(a == 0) return true;
+        else return false;
+    }
 }
